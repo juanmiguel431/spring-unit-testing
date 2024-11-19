22ae49747e2eff75649366418de4486223a05383
juanmiguel431 <juanmiguel431@gmail.com>
2024-11-18 22:11:30 -0400

    Some base classes added.

--- src/main/java/com/spring_unit_testing/demo/DemoApplication.java
+++ src/main/java/com/spring_unit_testing/demo/DemoApplication.java
@@ -2,6 +2,9 @@ package com.spring_unit_testing.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Scope;
+import com.spring_unit_testing.demo.models.CollegeStudent;
 
 @SpringBootApplication
 public class DemoApplication {
@@ -10,4 +13,9 @@ public class DemoApplication {
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
+	@Bean(name = "collegeStudent")
+	@Scope(value = "prototype")
+	CollegeStudent getCollegeStudent() {
+		return new CollegeStudent();
+	}
 }
---
+++ src/main/java/com/spring_unit_testing/demo/models/Student.java
@@ -0,0 +1,8 @@
+package com.spring_unit_testing.demo.models;
+
+public interface Student {
+
+    String studentInformation();
+
+    String getFullName();
+}
